151aab19f12210057ee19098e52fa74fb6889cc2
hector-kleeen <hector@kleeen.software>
2023-12-09 16:44:07 -0600

    test

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  distDir: 'dist',
 };
 
 module.exports = nextConfig;
