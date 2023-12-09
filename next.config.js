5a20e8aa866300936b30998c9e2eb4074e2b189c
hector-kleeen <hector@kleeen.software>
2023-12-09 16:31:50 -0600

    allow cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'false' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date',
+          },
+        ],
+      },
+    ];
+  },
   output: 'export',
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
