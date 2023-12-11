2974a12f348603ba4629aa2567999401be3f166b
hector-kleeen <hector@kleeen.software>
2023-12-11 16:28:38 -0600

    upgrate nextjs to v13

--- next.config.js
+++ next.config.js
@@ -2,42 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'false' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,DELETE,PATCH,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date',
-          },
-        ],
-      },
-    ];
-  },
-  output: 'export',
-  eslint: {
-    dirs: ['.'],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-  // output directory `out` -> `dist`
-  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
