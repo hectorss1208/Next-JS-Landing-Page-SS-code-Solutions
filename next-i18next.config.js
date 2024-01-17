e265e21a4f019a58f2a140e79505ff67898291af
hector-kleeen <hector@kleeen.software>
2024-01-17 12:33:02 -0600

    update next i18 config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,12 @@
 // next-i18next.config.js
 module.exports = {
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "es"],
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    localeDetection: true,
   },
+  // eslint-disable-next-line no-undef
+  localePath: path.resolve('./public/locales'),
   react: {
     useSuspense: false,
   },
