2e008196b110e7aee47892dca797703743931aed
hector-kleeen <hector@kleeen.software>
2024-01-17 12:01:47 -0600

    update pnpm lock and add cors

--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
 /**
  * @type {import('next').NextConfig}
  */
-const { i18n } = require("./next-i18next.config");
+const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'false' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date',
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     unoptimized: true,
