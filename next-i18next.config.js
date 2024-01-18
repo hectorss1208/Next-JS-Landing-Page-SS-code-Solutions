05268c4624b38a28a2e353368edcf8a687362a2d
hector-kleeen <hector@kleeen.software>
2024-01-17 18:25:44 -0600

    test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'es',
     locales: ['en', 'es'],
   },
   localePath: path.resolve('./public/locales'),
